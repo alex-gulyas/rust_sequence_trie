17ca442f00f57f94bb2db6d875488b7bf86015bf
Michael Sproul <micsproul@gmail.com>
2015-06-17 05:41:27 +0000

    Rebuild pages at 1cd9529.

--- implementors/core/clone/trait.Clone.js
+++ implementors/core/clone/trait.Clone.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['sequence_trie'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;K, V&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='sequence_trie/struct.SequenceTrie.html' title='sequence_trie::SequenceTrie'>SequenceTrie</a>&lt;K, V&gt;",];
+implementors['sequence_trie'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;K: <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a>, V: <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='sequence_trie/struct.SequenceTrie.html' title='sequence_trie::SequenceTrie'>SequenceTrie</a>&lt;K, V&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
